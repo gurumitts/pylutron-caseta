7c0d27ecf5b2e8a14840614f08b0c99217a7e403
gurumitts <miteshcoder1001@gmail.com>
2017-03-11 11:07:53 -0600

    saving work

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name="pylutron_caseta",
+    version="0.1.0",
+    license="Apache",
+    description="""Provides an API to the Lutron Smart Bridge""",
+    author="gurumitts",
+    author_email="",
+    maintainer="guumitts",
+    maintainer_email="",
+    platforms=["Linux"],
+    url="https://github.com/gurumitts/pylutron-caseta",
+    download_url="https://github.com/gurumitts/pylutron-caseta",
+    packages=["pylutron_caseta"]
+)
+
