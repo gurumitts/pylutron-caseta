13595c70e546471a2cd8568978f0853411b47f07
gurumitts <miteshcoder1001@gmail.com>
2017-07-28 12:24:49 -0500

    bumps version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="pylutron_caseta",
-    version="0.2.6",
+    version="0.2.7",
     license="Apache",
     description="""Provides an API to the Lutron Smartbridge""",
     author="gurumitts",
