0c56bacab76bca59f8cb7d3a8b5c066a7e53bcb1
Matthew Donoughe <mdonoughe@gmail.com>
2022-02-01 18:12:49 -0500

    release 0.13.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="pylutron_caseta",
-    version="0.13.0",
+    version="0.13.1",
     license="Apache",
     description="""Provides an API to the Lutron Smartbridge""",
     author="gurumitts",
