faeb9f010e8a3bb710aa68ded048c42632add20b
J. Nick Koston <nick@koston.org>
2023-03-07 12:33:01 -1000

    lint

--- pylutron_caseta/utils.py
+++ pylutron_caseta/utils.py
@@ -1,6 +1,10 @@
 import sys
 
+
 if sys.version_info[:2] < (3, 11):
     from async_timeout import timeout as asyncio_timeout
 else:
     from asyncio import timeout as asyncio_timeout
+
+
+__all__ = ["asyncio_timeout"]
