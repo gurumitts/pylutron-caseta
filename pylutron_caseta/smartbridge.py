5a39236682c3ae47e17b2a7ba35dd7571fe2fa32
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-08-04 03:39:24 +0200

    Remove deprecated ssl.PROTOCOL_TLSv1_2 (#143)

--- pylutron_caseta/smartbridge.py
+++ pylutron_caseta/smartbridge.py
@@ -108,7 +108,8 @@ class Smartbridge:
     @classmethod
     def create_tls(cls, hostname, keyfile, certfile, ca_certs, port=LEAP_PORT):
         """Initialize the Smart Bridge using TLS over IPv4."""
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
         ssl_context.load_verify_locations(ca_certs)
         ssl_context.load_cert_chain(certfile, keyfile)
         ssl_context.verify_mode = ssl.CERT_REQUIRED
